384851635500e8dffbf004c5ee7677f85fce742f
Yuriy Rusinov <yrusinov@gmail.com>
2013-03-18 16:12:52 +0400

    Some debug additions.
    Signed-off-by: Yuriy Rusinov <yrusinov@gmail.com>

--- src/fft_c.cpp
+++ src/fft_c.cpp
@@ -15,30 +15,39 @@ complex<long double> * FFT_Transform :: operator () (complex<long double> * src,
     if (n2 <=0 )
         return 0;
     complex<long double> * res = new complex<long double> [n2];
-    complex<long double> * in = new complex<long double> [n2];
+    //complex<long double> * in = new complex<long double> [n2];
+    fftwl_complex * in = (fftwl_complex*) fftwl_malloc (sizeof(fftwl_complex) * n2);
 
     if (nsrc > n2)
     {
         delete [] res;
-        delete [] in;
+        //delete [] in;
+        //fftw_free(in);
         return 0;
     }
     for (int i=0; i<nsrc; i++)
-        in[i] = src[i];
+    {
+        in[i][0] = real (src[i]);
+        in[i][1] = imag (src[i]);
+    }
 
     for (int i=nsrc; i<n2; i++)
-        in[i] = complex<long double> (0.0, 0.0);
+    {
+        in[i][0] = 0.0;
+        in[i][1] = 0.0;
+    }
 
     fftwl_plan p;
-    //fftwl_complex * out = (fftwl_complex*) fftwl_malloc (sizeof(fftwl_complex) * n2);
-    p = fftwl_plan_dft_1d (n2, reinterpret_cast<fftwl_complex*>(in), reinterpret_cast<fftwl_complex*>(res), sign, flags);
+    fftwl_complex * out = (fftwl_complex*) fftwl_malloc (sizeof(fftwl_complex) * n2);
+    p = fftwl_plan_dft_1d (n2, in, out, sign, flags);
     fftwl_execute (p);
-//    for (int i=0; i<n2; i++)
-//    {
-//        qDebug () << __PRETTY_FUNCTION__ << (double)real(res[i]) << (double)imag(res[i]);
-//    }
+    for (int i=0; i<n2; i++)
+    {
+        res[i] = complex<long double> (out[i][0], out[i][1]);
+        qDebug () << __PRETTY_FUNCTION__ << (double)real(res[i]) << (double)imag(res[i]);
+    }
     fftwl_destroy_plan (p);
-    delete [] in;
+//    delete [] in;
     return res;
 }
 
