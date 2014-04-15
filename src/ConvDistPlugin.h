71611d8d51ff0adbfe0419e4b56bb40dec75179d
Yuriy Rusinov <yrusinov@gmail.com>
2014-04-15 16:43:51 +0400

    New files for plugin.
    Signed-off-by: Yuriy Rusinov <yrusinov@gmail.com>

---
+++ src/ConvDistPlugin.h
@@ -0,0 +1,16 @@
+#ifndef _ConvDistPlugin_H
+#define _ConvDistPlugin_H
+
+#include "src_config.h"
+
+class _FFTMATR_EXPORT_ ConvDistInterface
+{
+public:
+    virtual ~ConvDistInterface (void);
+};
+
+class _FFTMATR_EXPORT_ ConvDistPlugin
+{
+};
+
+#endif
